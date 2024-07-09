9554c7d80db65b4ef8b29162d64bc3cdb74e0713
root <you@example.com>
2024-07-09 06:44:52 +0000

    Commiting the files

---
+++ index.js
@@ -0,0 +1,7 @@
+const { addDays } = require("date-fns");
+const getDateAfterXDays = (days) => {
+  const result = addDays(new Date(2020, 7, 22), days);
+  return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`;
+};
+
+module.exports = getDateAfterXDays;
